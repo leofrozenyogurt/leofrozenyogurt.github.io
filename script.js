8154dc97dc5b48243b648feee0b62080148bfda5
Leo Royzengurt <rebellabs@gmail.com>
2014-04-27 21:12:16 -0400

    home page layout

--- script.js
+++ script.js
@@ -22,12 +22,15 @@
 				templateUrl : 'pages/contact.html',
 				controller  : 'contactController'
 			});
+
+
 	});
 
 	// create the controller and inject Angular's $scope
 	scotchApp.controller('mainController', function($scope) {
 		// create a message to display in our view
 		$scope.message = 'Everyone come and see how good I look!';
+		
 	});
 
 	scotchApp.controller('aboutController', function($scope) {
