d986b3374a459a47c78f06e6b02d2503908eead8
Leo Royzengurt <rebellabs@gmail.com>
2014-05-15 14:32:45 -0400

    gh-pages

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
+var express        = require('express');
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var app            = express();
 
-	// set up ========================
-	var express  = require('express');
-	var app      = express(); 								// create our app w/ express
+app.use(express.static(__dirname + '/')); 	// set the static files location /public/img will be /img for users
+app.use(morgan('dev')); 					// log every request to the console
+app.use(bodyParser()); 						// pull information from html in POST
+app.use(methodOverride()); 					// simulate DELETE and PUT
 
-	// configuration =================
-
-		// connect to mongoDB database on modulus.io
-
-	app.configure(function() {
-		app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
-		app.use(express.logger('dev')); 						// log every request to the console
-		app.use(express.bodyParser()); 							// pull information from html in POST
-	});
-
-	// listen (start app with node server.js) ======================================
-	app.listen(8080);
-	console.log("App listening on port 8080");
+app.listen(8080);	
+console.log('Magic happens on port 8080'); 			// shoutout to the user
